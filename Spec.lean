b9c3755fb67ff6b352151dc9484637db6a1b03dc
Serhii Khoma <sergey@nordicresults.com>
2026-07-25 17:42:49 +0700

    feat: remove spec -> TODO use from lean-spec

--- Spec.lean
+++
@@ -1,6 +0,0 @@
-module
-prelude
-public import Spec.Assert
-public import Spec.Core
-public import Spec.Tree
-public import Spec.Reporter
--- Spec/Reporter.lean
+++
@@ -1,6 +0,0 @@
-module
-public import Spec.Reporter.Base
-public import Spec.Reporter.Console
-public import Spec.Reporter.Dot
-public import Spec.Reporter.Spec
-public import Spec.Reporter.Tap
